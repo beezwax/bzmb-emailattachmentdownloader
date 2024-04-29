b6ffc310f97149a165d7e147b625dad1feb082ed
Alec Gregory <alec_g@beezwax.net>
2024-04-29 23:15:44 +0100

    write test output to file

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
+
 const getAttachments = require("./getAttachments.js");
 const getMessagesWithAttachments = require("./getMessagesWithAttachments.js");
 
 const args = process.argv.slice(2);
 
-const [ path, host, port, user, pass, processedFolder, includeRead ] = args;
+const [ path, host, port, user, pass, processedFolder, includeRead, readOnly ] = args;
 
 (async () => {
 
@@ -20,20 +22,21 @@ const [ path, host, port, user, pass, processedFolder, includeRead ] = args;
       logger: false
     },
     processedFolder,
-    includeRead
+    includeRead,
+    readOnly
   }
   switch (path) {
     case "getAttachments":
       
       const attachments = await getAttachments(config);
+      fs.writeFileSync("testOutput/getAttachments_output.json", JSON.stringify(attachments, null, 2));
       console.log(`Downloaded ${attachments.length} attachment(s)`);
-      console.log(JSON.stringify(attachments, null, 2));
       break;
     case "getMessagesWithAttachments":
       
       const messages = await getMessagesWithAttachments(config);
+      fs.writeFileSync("testOutput/getMessagesWithAttachments_output.json", JSON.stringify(messages, null, 2));
       console.log(`Downloaded ${messages.length} message(s)`);
-      console.log(JSON.stringify(messages, null, 2));
       break;
   
     default:
